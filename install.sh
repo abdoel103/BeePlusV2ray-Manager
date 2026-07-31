2a6f6358fa4b26097f7eb0cca4fb6b78f99e25bb
abdoel103 <1dev1code@gmail.com>
2026-07-31 14:39:00 +0100

    update one line installer

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@
 set -e
 
 REPO="abdoel103/BeePlusV2ray-Manager"
-BRANCH="main"
+BRANCH="devlop"
 INSTALL_DIR="/usr/local/beeplus"
 
 echo "=========================================="
-echo "      BeePlusV2ray Manager Installer"
+echo "       BeePlusV2ray Manager Installer"
 echo "=========================================="
 
 # Root Check
@@ -17,12 +17,12 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 # Detect OS
-if [ -f /etc/os-release ]; then
-    . /etc/os-release
-else
+if [ ! -f /etc/os-release ]; then
     echo "[ERROR] Unsupported operating system."
     exit 1
 fi
+
+. /etc/os-release
 
 case "$ID" in
     debian|ubuntu)
@@ -33,20 +33,44 @@ case "$ID" in
         ;;
 esac
 
+
+echo
 echo "[INFO] Installing dependencies..."
 
 apt update
 
 apt install -y git curl wget unzip
 
+
+echo
 echo "[INFO] Downloading BeePlusV2ray Manager..."
 
 rm -rf "$INSTALL_DIR"
 
-git clone https://github.com/$REPO.git "$INSTALL_DIR"
+git clone -b "$BRANCH" https://github.com/$REPO.git "$INSTALL_DIR"
+
+
+echo
+echo "[INFO] Setting permissions..."
 
 chmod -R +x "$INSTALL_DIR"
+
+
+echo
+echo "[INFO] Starting installation..."
 
 cd "$INSTALL_DIR"
 
 bash installer/install.sh
+
+
+echo
+echo "=========================================="
+echo " Installation Completed Successfully!"
+echo "=========================================="
+
+echo
+echo "Run:"
+echo
+echo "bpm"
+echo
