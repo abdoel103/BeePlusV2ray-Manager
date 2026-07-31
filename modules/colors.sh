89e81ec4d2867c33cc7bb77c09ba9dbb4676d2e6
abdoel103 <1dev1code@gmail.com>
2026-07-31 12:58:07 +0100

    Update colors.sh

--- modules/colors.sh
+++ modules/colors.sh
@@ -1,1 +1,26 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# Colors
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+BLUE="\e[34m"
+CYAN="\e[36m"
+WHITE="\e[97m"
+RESET="\e[0m"
+
+function info() {
+    echo -e "${CYAN}$1${RESET}"
+}
+
+function success() {
+    echo -e "${GREEN}$1${RESET}"
+}
+
+function error() {
+    echo -e "${RED}$1${RESET}"
+}
+
+function warning() {
+    echo -e "${YELLOW}$1${RESET}"
+}
