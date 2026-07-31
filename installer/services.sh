64d97eff219e7d959e7bfb014842918a5323c00a
abdoel103 <1dev1code@gmail.com>
2026-07-31 14:02:25 +0100

    feat(installer): install websocket service

--- installer/services.sh
+++ installer/services.sh
@@ -1,1 +1,12 @@
+#!/usr/bin/env bash
 
+install_services() {
+
+    cp services/beeplus-websocket.service \
+       /etc/systemd/system/
+
+    systemctl daemon-reload
+
+    systemctl enable beeplus-websocket
+
+}
